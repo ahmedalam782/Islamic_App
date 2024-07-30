5b8fe73469f908ddf98b555b4e00dd8ab16ce403
ahmedalam <ahmedalam4887482@gmail.com>
2024-07-30 06:52:09 +0300

    add file of surah_details and hadith_details and show it in card

--- lib/Widgets/Tabs/Hadiths_Tab/Hadith_details/hadith_details.dart
+++ lib/Widgets/Tabs/Hadiths_Tab/Hadith_details/hadith_details.dart
@@ -1,18 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:islamic_app_route/Shared/Components/default_appbar.dart';
 import 'package:islamic_app_route/Shared/Components/default_background_image.dart';
 
 import '../../../../Models/hadith_model.dart';
 
-class HadithDetails extends StatelessWidget {
+class HadithDetails extends StatefulWidget {
   static const String routeName = "HadithDetails";
 
   const HadithDetails({super.key});
 
   @override
+  State<HadithDetails> createState() => _HadithDetailsState();
+}
+
+class _HadithDetailsState extends State<HadithDetails> {
+  List<String> hadithContent = [];
+
+  @override
   Widget build(BuildContext context) {
     HadithModel args =
         ModalRoute.of(context)?.settings.arguments as HadithModel;
+    if (hadithContent.isEmpty) {
+      readFiles(args.index!);
+    }
     return DefaultBackgroundImage(
       child: Scaffold(
         appBar: const DefaultAppbar(),
@@ -37,12 +48,51 @@ class HadithDetails extends StatelessWidget {
                   indent: MediaQuery.sizeOf(context).width * .12,
                   endIndent: MediaQuery.sizeOf(context).width * .12,
                   thickness: 1.3,
-                )
+                ),
+                SizedBox(
+                  height: MediaQuery.sizeOf(context).height * .03,
+                ),
+                hadithContent.isNotEmpty
+                    ? Expanded(
+                        child: ListView.separated(
+                          itemBuilder: (_, index) => Padding(
+                            padding: EdgeInsets.symmetric(
+                              horizontal:
+                                  MediaQuery.sizeOf(context).width * .08,
+                            ),
+                            child: Text(
+                              "${hadithContent[index]} ",
+                              textAlign: TextAlign.center,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleLarge
+                                  ?.copyWith(
+                                    color: Theme.of(context).primaryColor,
+                                    fontSize: 20,
+                                  ),
+                            ),
+                          ),
+                          separatorBuilder: (_, index) => SizedBox(
+                            height: MediaQuery.sizeOf(context).height * .04,
+                          ),
+                          itemCount: hadithContent.length,
+                        ),
+                      )
+                    : const Center(
+                        child: CircularProgressIndicator(),
+                      ),
               ],
             ),
           ),
         ),
       ),
     );
+  }
+
+  void readFiles(int chapterIndex) async {
+    String text = await rootBundle
+        .loadString('assets/files/hadiths/h${chapterIndex + 1}.txt');
+    hadithContent = text.split("\n");
+    setState(() {});
   }
 }
--- lib/Widgets/Tabs/Quran_Tab/Surah_details/surah_details.dart
+++ lib/Widgets/Tabs/Quran_Tab/Surah_details/surah_details.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:islamic_app_route/Models/alayat_model.dart';
 import 'package:islamic_app_route/Shared/Components/default_appbar.dart';
 import 'package:islamic_app_route/Shared/Components/default_background_image.dart';
 
-class SurahDetails extends StatelessWidget {
+class SurahDetails extends StatefulWidget {
   static const String routeName = "SurahDetails";
 
   const SurahDetails({super.key});
 
   @override
+  State<SurahDetails> createState() => _SurahDetailsState();
+}
+
+class _SurahDetailsState extends State<SurahDetails> {
+  List<String> chapterContent = [];
+
+  @override
   Widget build(BuildContext context) {
     AlayatModel args =
         ModalRoute.of(context)?.settings.arguments as AlayatModel;
+    if (chapterContent.isEmpty) {
+      readFiles(args.index!);
+    }
     return DefaultBackgroundImage(
       child: Scaffold(
         appBar: const DefaultAppbar(),
@@ -43,7 +54,7 @@ class SurahDetails extends StatelessWidget {
                         Icons.play_circle,
                         color: Theme.of(context).primaryColor,
                         size: MediaQuery.sizeOf(context).height * .04,
-                      )
+                      ),
                     ],
                   ),
                 ),
@@ -52,12 +63,51 @@ class SurahDetails extends StatelessWidget {
                   indent: MediaQuery.sizeOf(context).width * .12,
                   endIndent: MediaQuery.sizeOf(context).width * .12,
                   thickness: 1.3,
-                )
+                ),
+                SizedBox(
+                  height: MediaQuery.sizeOf(context).height * .03,
+                ),
+                chapterContent.isNotEmpty
+                    ? Expanded(
+                        child: ListView.separated(
+                          itemBuilder: (_, index) => Padding(
+                            padding: EdgeInsets.symmetric(
+                              horizontal:
+                                  MediaQuery.sizeOf(context).width * .08,
+                            ),
+                            child: Text(
+                              "(${index + 1})${chapterContent[index]} ",
+                              textAlign: TextAlign.center,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleLarge
+                                  ?.copyWith(
+                                    color: Theme.of(context).primaryColor,
+                                    fontSize: 20,
+                                  ),
+                            ),
+                          ),
+                          separatorBuilder: (_, index) => SizedBox(
+                            height: MediaQuery.sizeOf(context).height * .04,
+                          ),
+                          itemCount: chapterContent.length,
+                        ),
+                      )
+                    : const Center(
+                        child: CircularProgressIndicator(),
+                      ),
               ],
             ),
           ),
         ),
       ),
     );
+  }
+
+  void readFiles(int chapterIndex) async {
+    String text = await rootBundle
+        .loadString('assets/files/quran/${chapterIndex + 1}.txt');
+    chapterContent = text.split("\n");
+    setState(() {});
   }
 }
